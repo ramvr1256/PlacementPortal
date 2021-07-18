211a37ed33953e49e943841107192e4556cd1453
Ramveer <ramvr1256@gmail.com>
2021-07-18 12:30:04 +0530

    Code Upload

---
+++ app/config/constant.js
@@ -0,0 +1,23 @@
+// Constant Data - Being used all over the backend!
+module.exports = {
+    emailSuffix     	: 'iitk.ac.in',
+    programs            : ['UG','M.Tech.','M.Plan.','M.Sc.','PhD','MBA'],
+    placementTeamRoles  : ['spc', 'faculty-coordinator'],
+    coordinatorData     : {
+        degree          : 'N/A',
+        department      : 'N/A',
+        program         : 'N/A',
+        cgpa            : 'N/A',
+        alternate_email : 'N/A',
+        address         : '',
+        city            : '',
+        state           : '',
+        post_code       : '',
+        country         : 'India',
+        linkedln_link   : 'N/A',
+        matric_marks    : 'N/A',
+        matric_board    : 'N/A',
+        senior_marks    : 'N/A',
+        senior_board    : 'N/A'
+    }
+};
